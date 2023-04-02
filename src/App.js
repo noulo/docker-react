f7439793c55a5dad693970e465b4e82fba7a4ca1
Thanasis Noulas <thanasis.noulas@traderepublic.com>
2023-04-02 17:28:46 +0200

    new feature branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Hi there! I was changed on the feature Branch!
+          Hi there! I was changed on the new feature branch yo.
         </p>
         <a
           className="App-link"
